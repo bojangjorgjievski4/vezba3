f95988ddaf4c00cc8f9284a06af1a5489fcfa2d6
bojangjorgjievski4 <bojangjorgjievski399@gmail.com>
2023-11-30 09:39:56 +0100

    first commit

---
+++ proekt_31_x2.java
@@ -0,0 +1,48 @@
+package project_31_10_2;
+
+import java.util.Scanner;
+
+public class proekt_31_x2 {
+
+public static void main(String[] args) {
+
+Scanner tastatura = new Scanner(System.in);
+
+int a,b,c,d;
+
+int k=1;
+
+System.out.println("Vnesi vrednost za A: ");
+
+a=tastatura.nextInt();
+
+System.out.println("Vnesi vrednost za B: ");
+
+b=tastatura.nextInt();
+
+System.out.println("Vnesi vrednost za C: ");
+
+c=tastatura.nextInt();
+
+if(a<=5 && b<(a-c))
+
+{
+
+System.out.println(k);
+
+}
+
+else if(c == 0)
+
+{
+
+d=0;
+
+System.out.println(d);
+
+}
+
+}
+
+
+}
